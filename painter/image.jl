58e5c1e7887102cd0d1c6c233a7de0e0a5fb6134
Felix Eckhofer <felix@eckhofer.com>
2017-08-25 22:10:05 +0200

    Clean up code

--- painter/image.jl
+++ painter/image.jl
@@ -7,20 +7,26 @@ function draw()
    else
       filename = "images/stunet.png"
    end
-   stunet = load(filename)
+   stunet = load(filename)'
+   sizeX = size(stunet, 1)
+   sizeY = size(stunet, 2)
    while true
-      centerX = rand(1:(1920-size(stunet,2)))
-      centerY = rand(1:(1080-size(stunet,1)))
+      centerX = rand(1:(1920-sizeX))
+      centerY = rand(1:(1080-sizeY))
       shift = rand(1:360)
       println("Drawing $filename at $centerX $centerY")
+      # Draw twice for more drawing power
       for _ = 1:2
-         for x = shuffle(1:size(stunet, 2)), y = shuffle(1:size(stunet, 1))
-            color = stunet[y,x]
+         for x = shuffle(1:sizeX), y = shuffle(1:sizeY)
+            color = stunet[x,y]
             if convert(RGBA, color).alpha == 0 || color == RGB(1,1,1)
-               color = HSV(shift, 1, 1)
+               color = HSV(shift, 1, 1) # Change background
+               # continue # Transparent Background
             end
+            # Shift color for more colors
             color = convert(HSV, color)
             color = HSV(mod(color.h + shift, 360), color.s, color.v)
+            # Convert to hex
             color = convert(RGB24, color).color
             put!(pixel, "PX $(centerX+x) $(centerY+y) $(hex(color,6))")
          end
